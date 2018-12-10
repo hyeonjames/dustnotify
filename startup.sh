682c22a24992c98c013bcbb68b8077fc161b3e9a
김승현 <tmdgus0118@gmail.com>
2018-12-10 15:36:48 +0900

    startup.sh 수정

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p logs
 docker-compose down >> logs/`date +%Y-%m-%d.log`
-docker-compose up >> logs/`date +%Y-%m-%d.log` 2>>`date +%Y-%m-%d.err.log`
+docker-compose up >> logs/`date +%Y-%m-%d.log` 2>>`date +%Y-%m-%d.err.log` &

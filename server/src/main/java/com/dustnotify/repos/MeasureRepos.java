24c7a9dfad5b6f3927faeac1df08edf6ced8c785
PARK Yong Seo <j@saro.me>
2020-01-13 02:48:49 +0900

    Update MeasureRepos.java
    see https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods.query-creation
    see https://github.com/anissia-net/anissia-core/blob/master/src/main/kotlin/anissia/repository/UserRegisterAuthRepository.kt
    테스트를 하지 않았음

--- server/src/main/java/com/dustnotify/repos/MeasureRepos.java
+++ server/src/main/java/com/dustnotify/repos/MeasureRepos.java
@@ -13,6 +13,8 @@ public interface MeasureRepos extends JpaRepository<Measure,Long> {
     public Measure findByStationAndDate(Station st, Date dt);
 
     public List<Measure> findByStation(Station st);
-    @Query("SELECT (CASE WHEN COUNT(d) > 0 THEN TRUE ELSE FALSE END) FROM Measure d WHERE d.station = ?1 AND d.date >= ?2")
-    public boolean existDate(Station st, Date dt);
+    
+    //@Query("SELECT (CASE WHEN COUNT(d) > 0 THEN TRUE ELSE FALSE END) FROM Measure d WHERE d.station = ?1 AND d.date >= ?2")
+    //public boolean existDate(Station st, Date dt);
+    public boolean existsByStationAndDateGreaterThanEqual(Station station, Date date);
 }
